e5afa919843bcd569a1d4a8fa9d7e906f6b613d1
Micael Vitor <micael_vitor@yahoo.com.br>
2025-06-27 14:48:30 -0300

    feat: 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import { Toaster } from "sonner"
 import { Layout } from "@/components/Layout"
-import { MonstersPage } from "@/pages/MonstersPage"
-import { CreateMonsterPage } from "@/pages/CreateMonsterPage"
+import { MonsterList } from "@/features/monster/MonsterList"
+import { MonsterForm } from "@/features/monster/MonsterForm"
+import { NotFound } from "@/pages/NotFound"
+import { BattlePage } from "@/pages/Battle"
 
-function App() {
+export function App() {
   return (
     <Router>
       <Layout>
         <Routes>
           <Route path="/" element={<>'Salve'</>} />
-          <Route path="/monsters" element={<MonstersPage />} />
-          <Route path="/create" element={<CreateMonsterPage />} />
+          <Route path="/monsters" element={<MonsterList />} />
+          <Route path="/create" element={<MonsterForm />} />
+          <Route path="/battle" element={<BattlePage />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
-      <Toaster />
     </Router>
   )
 }
-
-export default App
